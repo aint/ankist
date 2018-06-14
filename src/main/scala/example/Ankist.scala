1b876318002cda56bc2656ea60f2e0fe8f7f3aef
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2018-06-14 23:57:55 +0300

    Replace Vertx's HTTP client with Web client

--- src/main/scala/example/Ankist.scala
+++ src/main/scala/example/Ankist.scala
@@ -6,14 +6,19 @@ import java.nio.file.Paths
 import com.softwaremill.sttp._
 import example.json.LinguaLeoResponse
 import io.vertx.scala.core.Vertx
+import io.vertx.scala.ext.web.client.WebClient
 import org.json4s._
 import org.json4s.DefaultFormats
 import org.json4s.native.JsonMethods
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.util.{Failure, Success}
+
 object Ankist {
 
   val USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
-  val LINGUA_LEO_API = "https://api.lingualeo.com/gettranslates"
+  val LINGUA_LEO_API = "http://api.lingualeo.com/gettranslates"
   val GOOGLE_TRANSLATE_BASE_URL = "translate.google.com"
   val GOOGLE_TRANSLATE_API = "/translate_tts?ie=UTF-8&tl=en-US&client=tw-ob"
 
@@ -21,43 +26,49 @@ object Ankist {
   implicit val formats = DefaultFormats
 
   private val vertx = Vertx.vertx()
-  private val client = vertx.createHttpClient()
+  private val client = WebClient.create(vertx)
 
   def main(args: Array[String]): Unit = {
     val word = "comprehension"
 
-    wordToMp3(downloadFile, word)
+    wordToMp3(word)
 
     val leoResponse = getLinguaLeoResponse(word)
     println(leoResponse)
   }
 
-  private def wordToMp3(fun: String => Array[Byte], word: String): Unit = {
-    val bytes = fun(s"$GOOGLE_TRANSLATE_API&q=$word")
-    val path = Paths.get(s"$word.mp3")
-    Files.write(path, bytes)
   }
 
   private def wordToPng(fun: String => Array[Byte], pngUrl: Option[String], word: String): Unit = {
-    val bytes = fun(pngUrl.get)
-    val path = Paths.get(s"$word.png")
-    Files.write(path, bytes)
+  private def wordToMp3(word: String): Unit = {
+    client.get(GOOGLE_TRANSLATE_BASE_URL, s"$GOOGLE_TRANSLATE_API&q=$word")
+      .sendFuture()
+      .map(_.bodyAsBuffer)
+      .onComplete{
+        case Success(result) =>
+          val bytes = result.get.getBytes
+          val path = Paths.get(s"$word.mp3")
+          Files.write(path, bytes)
+        case Failure(cause) =>
+          println(s"$cause")
+      }
   }
 
-  private def downloadFile(url: (String, String)): Array[Byte] = {
-    var res: Array[Byte] = null
-    client.getNow(url._1, url._2, response => {
-      println(s"Received response with status code ${response.statusCode()}")
-      response.bodyHandler(h => {
-        res = h.getByteBuf.array()
-      })
-    })
-
-    res
-    //TODO add USER_AGENT
-    //TODO handle exception
-    //TODO return Option
   }
+
+//  private def downloadFile(url: (String, String)): Array[Byte] = {
+//      client
+//        .get(url._1, url._2)
+//        .send(ar => {
+//          if (ar.failed()) {
+//            val bytes = ar.result.bodyAsBuffer().get.getBytes
+//          }
+//          else ar.cause.printStackTrace()
+//        })
+//    //TODO add USER_AGENT
+//    //TODO handle exception
+//    //TODO return Option
+//  }
 
   private def getLinguaLeoResponse(word: String): LinguaLeoResponse = {
     def parseJson(json: String): LinguaLeoResponse = JsonMethods.parse(json).camelizeKeys.extract[LinguaLeoResponse]
